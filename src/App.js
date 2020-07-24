2031f98d8c2138fe8f3fcfa90a4d8a90385be81f
guilherme <gui.mauricio@hotmail.com.br>
2020-07-24 18:03:06 -0300

    Api inclusa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Field, Form, } from 'formik';
 import './App.css';
 import schema from './schema';
 
@@ -8,39 +8,71 @@ function App() {
   function onSubmit(values,actions){
     console.log('usuario', values)
   }
-
-
+  function onBlurCep(ev, setFieldValue){
+    const  {value}=ev.target;
+    const cep = value.replace(/[^0-9]/, '')
+    if (cep?.length !== 8){
+      return ;
+    }
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        setFieldValue('bairro',data.bairro);
+         setFieldValue('cidade', data.localidade);
+      setFieldValue('logradouro', data.logradouro);
+         setFieldValue('uf',data.uf)
+      });
+  }
+  
   
   return (
     <div className="App">
-      <Formik 
-      validationSchema={schema}
-      onSubmit= {onSubmit}
-      validateOnMount
-      initialValues={{
-        name: '',
-        email: '',
-      }}
-      render={({ values,errors,touched,isvalid })=>(
-   <Form>
-        <div>
-            <label>nome</label>
-            <Field name="name" type=" text" />
-            <ErrorMessage name="name" />
-          </div>
-
-          <div>
-            <label>email</label>
-            <Field name="email" type="email" />
-            <ErrorMessage name="email" />
-          </div>
-        <button type="submit" disabled={!isvalid} >enviar</button>
+    <Formik 
+    validationSchema={schema}
+    onSubmit= {onSubmit}
+    validateOnMount
+    initialValues={{
+      name: '',
+      email: '',
+    }}
+    render={({ isvalid,setFieldValue })=>(
+      <Form>
+        <div className="form-control-group">
+          <label>Cep</label>
+          <Field name="cep" type=" text" onBlur={(ev)=>onBlurCep(ev, setFieldValue)}/>     
+        </div>
+        <div className="form-control-group">
+          <label>Logradouro</label>
+          <Field name="logradouro" type=" text" />
+        </div>
+         <div className="form-control-group">
+          <label>Número</label>
+         <Field name="numero" type=" text" />
+         </div>
+         <div className="form-control-group">
+          <label>Bairro</label>
+           <Field name="bairro" type=" text" />
+        </div>          
+        <div className="form-control-group">
+         <label>Cidade</label>
+           <Field name="cidade" type=" text" />
+       </div>
+         <div className="form-control-group">
+             <label>Estado</label>
+             <Field  name="uf" type=" text"/>
+            
+                    
+        </div>
+      
+      
+      <button type="submit" >enviar</button>
       </Form>
-        
+      
       )}
       />
       </div>
-  );
-}
-
-export default App;
+      );
+    }
+    
+    export default App;
+    
--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup'
 
 export default Yup.object().shape({
-    name: Yup.string().required(),
+    cep: Yup.string().min(2).required(),
     email: Yup.string().email().required()
 })
